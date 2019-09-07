7c78fe03e27e8a8815d1909f96d90f321f8ed184
Brittany Heller <brittany.heller@live.com>
2019-09-07 09:21:32 -0700

    initial commit of project api

---
+++ src/projectApi.Common/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectApi.Common.Models
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public String ProjectLink { get; set; }
+        public String GitHubLink { get; set; }
+        public int Votes { get; set; }
+        public bool Completed { get; set; }
+    }
+}
